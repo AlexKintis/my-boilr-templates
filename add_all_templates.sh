0ce8e7c5f736b04e25fbca0e586dc77935936bd7
AlexKintis <kintisalexandros@gmail.com>
2025-12-28 16:51:50 +0200

    updates in script which adds the boilerplates

--- add_all_templates.sh
+++ add_all_templates.sh
@@ -7,33 +7,23 @@
 
 # This script will add or update all templates in the current directory and its subdirectories
 process_directory() {
+  local dir="$1"
 
-    local dir="$1"
+  for d in "$dir"/*; do
+    [[ -d "$d" ]] || continue
 
-    for d in "$dir"/*; do
+    if [[ -d "$d/template" && -f "$d/project.json" ]]; then
+      local template_name
+      template_name="$(basename "$d")"
 
-        if [ -d "$d" ]; then
+      echo "Adding template: $d"
+      echo "Template name: $template_name"
 
-            if [ -d "$d/template" ] || [ -f "$d/project.json" ]; then
-
-                echo "Adding template: $d"
-                template_name=$(basename "$d")
-                echo "With template name: $d"
-
-                # Save the template to boilr
-                boilr template save -f "$d" "$template_name"
-
-            else
-
-                echo "Entering $d"
-                cd "$d" || exit
-                cd ..
-                process_directory "$d"
-
-            fi
-        fi
-    done
+      boilr template save -f "$d" "$template_name"
+    else
+      process_directory "$d"
+    fi
+  done
 }
 
-# Start processing the current directory
 process_directory .
